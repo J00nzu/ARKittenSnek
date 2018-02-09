3fd97c232a1d6895e2599b398d8b982239dfbae0
ilppi <jonne.leinonen@metropolia.fi>
2018-02-09 10:26:25 +0200

    GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	int foodCount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ScoreCounter() {
+		foodCount++;
+	}
+}
