2fc1e9491b855d9bbd3c32e3035bab37fa41375c
ilppi <jonne.leinonen@metropolia.fi>
2018-02-09 09:49:41 +0200

    korjus

--- Assets/Scripts/EatScript.cs
+++ Assets/Scripts/EatScript.cs
@@ -12,18 +12,18 @@ public class EatScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//ku se syö sen nii kutsuu ohjelman jostain
-		if (coll.collider.tag.Equals("food")) {
+		/*if (coll.collider.tag.Equals("food")) {
 			Destroy(coll.gameObject);
 			PC.ScoreCounter ();
-		}
+		}*/
 	}
 	//Haluutaanko me sitä pistemäärää siihen ruutu tielle?
 	//mut tää niku tallentais sen iha simppelisti ja sitä voi sit käyttää ku peli loppuu etc
-	public void ScoreCounter() {
+	/*public void ScoreCounter() {
 		int foodCount;
 		foodCount++;
 		counter.text = "x " + foodCount;
-	}
+	}*/
 }
 	
 // Jotain tämmöst, mul ei oo hajuu miten noi scriptit menee nii pitää vaa inserttaa nää oikeisii paikkoihi
